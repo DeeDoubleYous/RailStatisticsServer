1ccf1e2bc314e2cbb61dccaadf1e5df5585dea3a
DeeDoubleYous <wishart.a470@gmail.com>
2022-03-17 16:12:07 +0000

    Add totals check

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,21 @@
 import express from 'express';
 import { json, urlencoded } from 'body-parser';
-import { readLog } from './handlers/readLog';
+import { readLog, readTotal } from './handlers/readLog';
 import { logUse } from './handlers/logUse';
 
 const app = express();
 
 const port = 8080;
+const appName = `railvisstats`;
 
 app.use(json());
 app.use(urlencoded());
 
-app.post('/railvisstats/log', logUse);
+app.post(`/${appName}/log`, logUse);
 
-app.get('/railvisstats/log', readLog);
+app.get(`/${appName}/log`, readLog);
+
+app.get(`/${appName}/log/total`, readTotal);
 
 app.listen(port, () => {
     console.log(`App is listening on http://localhost:${port}`);
--- src/handlers/readLog.ts
+++ src/handlers/readLog.ts
@@ -6,20 +6,42 @@ export const readLog = async (req: Request, res: Response) => {
     if(!checkKey(req.query.key as string)){
         res.status(403);
         res.end();
-    }else{
-        try{
-            const sql = 'CALL Logging_GetTotals()';
-    
-            const results = await executeQuery(sql, []);
-    
-            if(results){
-                res.status(200);
-                res.json(results);
-            }
-        }catch(e){
-            console.error(e);
-            res.status(500);
-            res.end();
+        return;
+    }
+    try{
+        const sql = 'CALL Logging_GetTotals()';
+
+        const results = await executeQuery(sql, []);
+
+        if(results){
+            res.status(200);
+            res.json(results);
         }
+    }catch(e){
+        console.error(e);
+        res.status(500);
+        res.end();
+    }
+};
+
+export const readTotal = async (req: Request, res: Response) => {
+    if(!checkKey(req.query.key as string)){
+        res.status(403);
+        res.end();
+        return;
+    }
+    try{
+        const sql = `CALL Logging_GetTotal()`;
+
+        const result = await executeQuery(sql, []);
+
+        if(result){
+            res.status(200);
+            res.json(result);
+        }
+    }catch(e){
+        console.error(e);
+        res.status(500);
+        res.end();
     }
 }
